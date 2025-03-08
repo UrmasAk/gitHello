e6419fd6159b480c2e602f46e837dfc656b7b779
Urmas Akerman <urmas.akerman@gmail.com>
2025-03-08 17:26:07 +0200

    stash done

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("friend still here, conflict");
+        System.out.printf("stash");
 
     }
 }
